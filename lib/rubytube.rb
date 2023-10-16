5c7875735ef3ddc21760fc5c5e48fcda43886fbb
nightswinger <stardustkids83@gmail.com>
2023-10-16 02:28:16 +0000

    add error

--- lib/rubytube.rb
+++ lib/rubytube.rb
@@ -7,6 +7,7 @@ require_relative 'rubytube/version'
 
 require_relative 'rubytube/cipher'
 require_relative 'rubytube/client'
+require_relative 'rubytube/error'
 require_relative 'rubytube/extractor'
 require_relative 'rubytube/innertube'
 require_relative 'rubytube/monostate'
@@ -18,49 +19,6 @@ require_relative 'rubytube/stream'
 require_relative 'rubytube/utils'
 
 module RubyTube
-  class Error < StandardError; end
-  class HTMLParseError < StandardError; end
-  class ExtractError < StandardError; end
-  class MaxRetriesExceeded < StandardError; end
-  class VideoUnavailable < StandardError; end
-  class InvalidArgumentError < ArgumentError; end
-
-  class RegexMatchError < StandardError
-    def initialize(caller, pattern)
-      super("Regex match error in #{caller} for pattern #{pattern}")
-    end
-  end
-
-  class MembersOnly < StandardError
-    def initialize(video_id)
-      super("Members only video: #{video_id}")
-    end
-  end
-  
-  class RecordingUnavailable < StandardError
-    def initialize(video_id)
-      super("Recording unavailable: #{video_id}")
-    end
-  end
-  
-  class VideoUnavailable < StandardError
-    def initialize(video_id)
-      super("Video unavailable: #{video_id}")
-    end
-  end
-  
-  class VideoPrivate < StandardError
-    def initialize(video_id)
-      super("Video is private: #{video_id}")
-    end
-  end
-  
-  class LiveStreamError < StandardError
-    def initialize(video_id)
-      super("Video is a live stream: #{video_id}")
-    end
-  end
-
   class << self
     def new(url)
       Client.new(url)
---
+++ lib/rubytube/error.rb
@@ -0,0 +1,44 @@
+module RubyTube
+  class Error < StandardError; end
+  class HTMLParseError < StandardError; end
+  class ExtractError < StandardError; end
+  class MaxRetriesExceeded < StandardError; end
+  class VideoUnavailable < StandardError; end
+  class InvalidArgumentError < ArgumentError; end
+
+  class RegexMatchError < StandardError
+    def initialize(caller, pattern)
+      super("Regex match error in #{caller} for pattern #{pattern}")
+    end
+  end
+
+  class MembersOnly < StandardError
+    def initialize(video_id)
+      super("Members only video: #{video_id}")
+    end
+  end
+  
+  class RecordingUnavailable < StandardError
+    def initialize(video_id)
+      super("Recording unavailable: #{video_id}")
+    end
+  end
+  
+  class VideoUnavailable < StandardError
+    def initialize(video_id)
+      super("Video unavailable: #{video_id}")
+    end
+  end
+  
+  class VideoPrivate < StandardError
+    def initialize(video_id)
+      super("Video is private: #{video_id}")
+    end
+  end
+  
+  class LiveStreamError < StandardError
+    def initialize(video_id)
+      super("Video is a live stream: #{video_id}")
+    end
+  end
+end
